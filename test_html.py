69467b2e3292d73a1d3dbfec666d948e5caabe6b
koalmsk <104090753+koalmsk@users.noreply.github.com>
2024-04-01 19:32:28 +0300

    рега

--- test_html.py
+++ test_html.py
@@ -3,11 +3,17 @@ import remote_api
 app = flask.Flask(__name__)
 
 
+# @app.route("/")
+# def test():
+#     data = remote_api.get_recipe("pasts")
+    
+#     return flask.render_template("single.html", **data[0])
+
 @app.route("/")
 def test():
-    data = remote_api.get_recipe("pasts")
+    # data = remote_api.get_recipe("pasts")
     
-    return flask.render_template("single.html", **data[0])
+    return flask.render_template("contact.html")
 
 
 
